feffd006b03474d2ae1f7184aa578b78c3fb38b0
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-03-31 23:19:04 +0700

    feat: Update icon and title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $title ?? 'HUTECH QUIZ' }}</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/logoHutech.ico') }}">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">

214f0647b44620b921ca271d9242bba047b5ad56
Slimaeus <nguyenhongthai28042002@gmail.com>
2024-04-02 23:28:24 +0700

    refactor: Use username for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,11 +32,11 @@
                                         class="bi bi-person-fill me-2"></i>{{ __('Tài khoản') }}</label>
 
                                 <div class="col-md-10">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <input id="username" type="text"
+                                        class="form-control @error('username') is-invalid @enderror" name="username"
+                                        value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                                    @error('email')
+                                    @error('username')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>

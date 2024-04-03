6e3b3048bd03dc0b6bd09bd9d60960ca1092576c
Slimaeus <nguyenhongthai28042002@gmail.com>
2024-04-03 09:02:14 +0700

    feat: Set username in user as unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

926d2b1408fffb93d32d5157566b1e8a55efe3f2
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-03 14:47:49 +0700

    feat: Update profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    //
+    /**
+     * Hiển thị trang hồ sơ cá nhân của người dùng.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
-        return view('profile.index');
+        // Lấy thông tin của người dùng hiện tại đã đăng nhập
+        $user = Auth::user();
+
+        // Trả về view 'profile.index' và truyền biến $user vào view
+        return view('profile.index', ['user' => $user]);
     }
 }

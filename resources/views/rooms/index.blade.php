62228109b775926cd4e5b2d3eae72d8a88143baa
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-07 22:00:44 +0700

    refactor: Change UI room list page

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -1,54 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
-        <h1>Rooms</h1>
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Code</td>
-                    <td>Owner</td>
-                    <td>Current Quiz</td>
-                    <td>Quiz Collection</td>
-                    <td>Actions</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($rooms as $key => $value)
-                    <tr>
-                        <td>{{ $value->id }}</td>
-                        <td>{{ $value->code }}</td>
-                        <td>{{ $value->owner->username }}</td>
-                        <td>{{ $value->currentQuiz ? $value->currentQuiz->content : '' }}</td>
-                        <td>{{ $value->quizCollection->name }}</td>
-
-                        <!-- we will also add show, edit, and delete buttons -->
-                        <td>
-
-                            <!-- delete the quiz (uses the destroy method DESTROY /rooms/{id} -->
-                            <!-- we will add this later since its a little more complicated than the other two buttons -->
-
-                            <!-- show the quiz (uses the show method found at GET /rooms/{id} -->
-                            <a class="btn btn-small btn-success" href="{{ URL::to('rooms/' . $value->id) }}">Show this
-                                quiz</a>
-
-                            <!-- edit this quiz (uses the edit method found at GET /rooms/{id}/edit -->
-                            <a class="btn btn-small btn-info"
-                                href="{{ URL::to('rooms/' . $value->id . '/edit') }}">Edit this quiz</a>
-
-                            <!-- delete this quiz -->
-
-                            <form action="{{ url('rooms/' . $value->id) }}" method="POST" class="pull-right">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-small btn-danger">Delete this quiz</button>
-                            </form>
-
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="container">
+        <h1 class="mb-4">DANH SÁCH PHÒNG</h1>
+        <div class="row mb-3">
+            <div class="col-md-6">
+                <a href="#" class="btn btn-primary">
+                    <i class="bi bi-plus-circle"></i> TẠO PHÒNG
+                </a>
+            </div>
+            <div class="col-md-6 text-end">
+                <a href="#" class="btn btn-secondary">
+                    <i class="bi bi-search"></i> TÌM PHÒNG
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            @foreach ($rooms as $room)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div class="icon bg-primary rounded-circle p-2">
+                                    <i class="bi bi-book"></i>
+                                </div>
+                                <h5 class="card-title ms-3 mb-0">{{ $room->code }}</h5>
+                            </div>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $room->owner->username }}</h6>
+                            <img src="{{ asset('images/banner-hutech-quiz.png') }}" class="img-fluid mb-3" alt="Banner">
+                            <a href="{{ URL::to('rooms/' . $room->id) }}" class="btn btn-primary">
+                                VÀO PHÒNG
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection

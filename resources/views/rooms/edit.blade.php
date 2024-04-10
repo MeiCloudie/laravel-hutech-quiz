50288e82bd4fc15c70cc88ee00be0eedeccaae3d
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-10 21:50:03 +0700

    feat: Update edit (problem)

--- resources/views/rooms/edit.blade.php
+++ resources/views/rooms/edit.blade.php
@@ -7,18 +7,21 @@
                 <h1 class="card-title fw-bold mt-2">CHỈNH SỬA PHÒNG THI</h1>
             </div>
             <div class="card-body">
-                <!-- Form for creating a room -->
-                <form action="{{ url('rooms') }}" method="POST">
+                <!-- Form for editing a room -->
+                <form action="{{ url('rooms/' . $room->id) }}" method="POST">
                     @csrf
+                    @method('PUT')
 
                     <!-- Select owner of the room -->
                     <div class="mb-3">
                         <label for="ownerId" class="form-label">Chủ Phòng</label>
                         <select id="ownerId" class="form-select @error('ownerId') is-invalid @enderror" name="ownerId"
                             required>
-                            <option value="" selected disabled>Chọn chủ phòng</option>
+                            <option value="" disabled>Chọn chủ phòng</option>
                             @foreach ($users as $owner)
-                                <option value="{{ $owner->id }}">{{ $owner->username }}</option>
+                                <option value="{{ $owner->id }}" {{ $owner->id == $room->ownerId ? 'selected' : '' }}>
+                                    {{ $owner->username }}
+                                </option>
                             @endforeach
                         </select>
                         @error('ownerId')
@@ -31,9 +34,12 @@
                         <label for="quizCollectionId" class="form-label">Bộ Đề Thi</label>
                         <select id="quizCollectionId" class="form-select @error('quizCollectionId') is-invalid @enderror"
                             name="quizCollectionId" required>
-                            <option value="" selected disabled>Chọn bộ đề thi</option>
+                            <option value="" disabled>Chọn bộ đề thi</option>
                             @foreach ($quizCollections as $quizCollection)
-                                <option value="{{ $quizCollection->id }}">{{ $quizCollection->name }}</option>
+                                <option value="{{ $quizCollection->id }}"
+                                    {{ $quizCollection->id == $room->quizCollectionId ? 'selected' : '' }}>
+                                    {{ $quizCollection->name }}
+                                </option>
                             @endforeach
                         </select>
                         @error('quizCollectionId')
@@ -43,7 +49,7 @@
 
                     <!-- Submit button -->
                     <button type="submit" class="btn btn-primary">CẬP NHẬT PHÒNG THI</button>
-                    <a href="{{ url('rooms') }}" class="btn btn-outline-secondary">VỀ DANH SÁCH</a>
+                    <a href="{{ URL::to('rooms/' . $room->id) }}" class="btn btn-outline-secondary">VỀ PHÒNG</a>
                 </form>
             </div>
         </div>

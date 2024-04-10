d289529787da63e9daedbb698f0608d6cad7592e
Slimaeus <nguyenhongthai28042002@gmail.com>
2024-04-10 21:20:46 +0700

    feat: Add role to user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'last_name',
         'username',
         'email',
+        'role',
         'password',
         'faculty_id'
     ];
@@ -34,6 +35,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+    ];
+
+    
+
+    protected $attributes = [
+        'role' => 'USER'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('role')->default('USER');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

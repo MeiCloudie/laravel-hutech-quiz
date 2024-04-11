61069e95b20f030f6b2b3985e7e20c14dafb4721
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-11 18:56:02 +0700

    feat: Test quiz answer

--- resources/views/play/index.blade.php
+++ resources/views/play/index.blade.php
@@ -70,6 +70,10 @@
                                                 {{ $quiz->content }}</h5>
                                             <p class="card-text">{{ $quiz->explaination }}</p>
                                         </div>
+                                        @foreach ($quiz->answers as $answer)
+                                            <div>{{ $answer->content }}</div>
+                                            <br />
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
@@ -97,92 +101,95 @@
             <hr class="mt-1" />
 
 
-        {{-- MODAL CHỈNH SỬA BỘ ĐỀ THI --}}
-        <div class="modal fade" id="editRoomModal" tabindex="-1" aria-labelledby="editRoomModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="editRoomModalLabel">CHỈNH SỬA BỘ ĐỀ THI</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        {{-- Form chỉnh sửa bộ đề thi --}}
-                        <form action="{{ url('rooms/' . $room->id) }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <div class="mb-3">
-                                <label for="quizCollectionId" class="form-label">Chọn Bộ Đề Thi</label>
-                                <select class="form-select" id="quizCollectionId" name="quizCollectionId" required>
-                                    <option value="" disabled selected>Chọn bộ đề</option>
-                                    @foreach ($quizCollections as $quizCollection)
-                                        <option value="{{ $quizCollection->id }}">{{ $quizCollection->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <button type="submit" class="btn btn-primary">CẬP NHẬT</button>
-                            <a href="{{ URL::to('rooms/' . $room->id . '/edit') }}" class="btn btn-outline-secondary">ĐẾN
-                                TRANG</a>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        {{-- MODAL XÁC NHẬN XOÁ --}}
-        <div class="modal fade" id="deleteRoomModal" tabindex="-1" aria-labelledby="deleteRoomModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="deleteRoomModalLabel">XÁC NHẬN XOÁ</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Bạn có chắc chắn muốn xoá phòng này không?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">HUỶ</button>
-                        <form action="{{ url('rooms/' . $room->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">XÁC NHẬN XOÁ</button>
-                        </form>
+            {{-- MODAL CHỈNH SỬA BỘ ĐỀ THI --}}
+            <div class="modal fade" id="editRoomModal" tabindex="-1" aria-labelledby="editRoomModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="editRoomModalLabel">CHỈNH SỬA BỘ ĐỀ THI</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            {{-- Form chỉnh sửa bộ đề thi --}}
+                            <form action="{{ url('rooms/' . $room->id) }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="mb-3">
+                                    <label for="quizCollectionId" class="form-label">Chọn Bộ Đề Thi</label>
+                                    <select class="form-select" id="quizCollectionId" name="quizCollectionId" required>
+                                        <option value="" disabled selected>Chọn bộ đề</option>
+                                        @foreach ($quizCollections as $quizCollection)
+                                            <option value="{{ $quizCollection->id }}">{{ $quizCollection->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">CẬP NHẬT</button>
+                                <a href="{{ URL::to('rooms/' . $room->id . '/edit') }}"
+                                    class="btn btn-outline-secondary">ĐẾN
+                                    TRANG</a>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        {{-- MODAL XÁC NHẬN BẮT ĐẦU --}}
-        <div class="modal fade" id="confirmStartModal" tabindex="-1" aria-labelledby="confirmStartModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="confirmStartModalLabel">XÁC NHẬN BẮT ĐẦU</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Bạn có chắc chắn muốn bắt đầu phòng thi này không?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">HUỶ</button>
-                        <a href="{{ URL::to('rooms/' . $room->id . '/play') }}" class="btn btn-success">
-                            BẮT ĐẦU
-                        </a>
+            {{-- MODAL XÁC NHẬN XOÁ --}}
+            <div class="modal fade" id="deleteRoomModal" tabindex="-1" aria-labelledby="deleteRoomModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="deleteRoomModalLabel">XÁC NHẬN XOÁ</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Bạn có chắc chắn muốn xoá phòng này không?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">HUỶ</button>
+                            <form action="{{ url('rooms/' . $room->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">XÁC NHẬN XOÁ</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            {{-- MODAL XÁC NHẬN BẮT ĐẦU --}}
+            <div class="modal fade" id="confirmStartModal" tabindex="-1" aria-labelledby="confirmStartModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="confirmStartModalLabel">XÁC NHẬN BẮT ĐẦU</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Bạn có chắc chắn muốn bắt đầu phòng thi này không?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">HUỶ</button>
+                            <a href="{{ URL::to('rooms/' . $room->id . '/play') }}" class="btn btn-success">
+                                BẮT ĐẦU
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <style>
+                .card {
+                    transition: box-shadow 0.3s ease;
+                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0);
+                }
+
+                .card:hover {
+                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+                }
+            </style>
         </div>
-
-        <style>
-            .card {
-                transition: box-shadow 0.3s ease;
-                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0);
-            }
-
-            .card:hover {
-                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-            }
-        </style>
-    </div>
-@endsection
+    @endsection

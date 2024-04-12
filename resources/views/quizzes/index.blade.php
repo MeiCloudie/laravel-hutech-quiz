0194ff7507a5f1a7d3db219890237f2cc7cea0f5
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-12 07:51:03 +0700

    refactor: Update UI quiz - index

--- resources/views/quizzes/index.blade.php
+++ resources/views/quizzes/index.blade.php
@@ -1,67 +1,52 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 
-<head>
-    <title>Quiz App</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-
-<body>
+@section('content')
     <div class="container">
-
-        <nav class="navbar navbar-inverse">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="{{ URL::to('quizzes') }}">Quiz Alert</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="{{ URL::to('quizzes') }}">View All quizzes</a></li>
-                <li><a href="{{ URL::to('quizzes/create') }}">Create a quiz</a>
-            </ul>
-        </nav>
-
-        <h1>All the quizzes</h1>
+        <div class="d-flex justify-content-between align-items-center">
+            <h1 class="fw-bold">DANH SÁCH BỘ CÂU HỎI</h1>
+            <a href="{{ URL::to('quizzes/create') }}" class="btn btn-primary"><i class="bi bi-plus-circle me-2"></i>TẠO BỘ CÂU
+                HỎI</a>
+        </div>
 
         <!-- will be used to show any messages -->
         @if (Session::has('message'))
-            <div class="alert alert-info">{{ Session::get('message') }}</div>
+            <div class="alert alert-info mt-3">{{ Session::get('message') }}</div>
         @endif
 
-        <table class="table table-striped table-bordered">
-            <thead>
+        <table class="table table-striped table-bordered mt-4">
+            <thead class="thead-dark">
                 <tr>
-                    <td>ID</td>
-                    <td>Content</td>
-                    <td>Explaination</td>
-                    <td>Actions</td>
+                    <th class="text-center">ID</th>
+                    <th class="text-center">Nội dung</th>
+                    <th class="text-center">Giải thích</th>
+                    <th class="text-center">Hành động</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($quizzes as $key => $value)
                     <tr>
-                        <td>{{ $value->id }}</td>
+                        <td class="text-center">{{ $value->id }}</td>
                         <td>{{ $value->content }}</td>
                         <td>{{ $value->explaination }}</td>
 
                         <!-- we will also add show, edit, and delete buttons -->
-                        <td>
-
-                            <!-- delete the quiz (uses the destroy method DESTROY /quizzes/{id} -->
-                            <!-- we will add this later since its a little more complicated than the other two buttons -->
+                        <td class="text-center">
 
                             <!-- show the quiz (uses the show method found at GET /quizzes/{id} -->
-                            <a class="btn btn-small btn-success" href="{{ URL::to('quizzes/' . $value->id) }}">Show this
-                                quiz</a>
+                            <a class="btn btn-success mr-2" href="{{ URL::to('quizzes/' . $value->id) }}"><i
+                                    class="bi bi-eye-fill me-2"></i>Xem</a>
 
                             <!-- edit this quiz (uses the edit method found at GET /quizzes/{id}/edit -->
-                            <a class="btn btn-small btn-info"
-                                href="{{ URL::to('quizzes/' . $value->id . '/edit') }}">Edit this quiz</a>
+                            <a class="btn btn-info mr-2" href="{{ URL::to('quizzes/' . $value->id . '/edit') }}"><i
+                                    class="bi bi-pen-fill me-2"></i>Chỉnh
+                                sửa</a>
 
                             <!-- delete this quiz -->
-
-                            <form action="{{ url('quizzes/' . $value->id) }}" method="POST" class="pull-right">
+                            <form action="{{ url('quizzes/' . $value->id) }}" method="POST" class="d-inline">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-small btn-danger">Delete this quiz</button>
+                                <button type="submit" class="btn btn-danger"><i
+                                        class="bi bi-trash-fill me-2"></i>Xoá</button>
                             </form>
 
                         </td>
@@ -71,6 +56,4 @@
         </table>
 
     </div>
-</body>
-
-</html>
+@endsection

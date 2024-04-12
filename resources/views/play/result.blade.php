fba0fa569bdfe2fe7985fc6de5061deaf24566ab
Slimaeus <nguyenhongthai28042002@gmail.com>
2024-04-12 09:36:38 +0700

    feat: Add get full name for user

--- resources/views/play/result.blade.php
+++ resources/views/play/result.blade.php
@@ -16,7 +16,7 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-body mb-0">
-                        <h4 class="card-title fw-bold mb-2">Họ tên: ...</h4>
+                        <h4 class="card-title fw-bold mb-2">Họ tên: {{ Auth::user()->getFullName() }}</h4>
                         <h5 class="card-text">Số câu đúng: ...</h5>
                         <h5 class="card-text">Số câu sai: ...</h5>
                         <h5 class="card-text fw-bold text-danger">TỔNG ĐIỂM: ...</h5>

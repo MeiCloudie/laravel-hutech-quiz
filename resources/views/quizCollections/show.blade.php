7424353c01a74e6cd40b8e236fe231a6cf35ef03
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-04-12 23:10:04 +0700

    feat: show quiz Col

--- resources/views/quizCollections/show.blade.php
+++ resources/views/quizCollections/show.blade.php
@@ -15,11 +15,33 @@
             </div>
         </div>
 
-        {{-- TODO: Chưa có list quiz --}}
-        <div>
-            @foreach ($quizCollection->quizzes as $quiz)
-                <div>{{ $quiz->quizToQuizCollection->order }}. {{ $quiz->content }}</div>
-            @endforeach
+        {{-- Hiển thị danh sách câu hỏi --}}
+        <div class="mt-4">
+            <div class="row row-cols-1">
+                <h3 class="fw-bold">Danh sách bộ câu hỏi:</h3>
+                @foreach ($quizCollection->quizzes as $quiz)
+                    <div class="col mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold mb-2">Câu {{ $quiz->quizToQuizCollection->order }}:
+                                    {{ $quiz->content }}</h5>
+                                <p class="card-text">{{ $quiz->explaination }}</p>
+                                <div class="list-group">
+                                    @foreach ($quiz->answers->sortBy(function ($item, $key) {
+            return $item->order;
+        }) as $answer)
+                                        <label class="list-group-item">
+                                            <input type="radio" disabled name="answers[{{ $quiz->id }}]"
+                                                value="{{ $answer->id }}">
+                                            {{ chr($answer->order + 64) }}. {{ $answer->content }}
+                                        </label>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
 
         <a class="btn btn-primary btn-lg mt-3" href="{{ URL::to('quizCollections') }}" role="button">VỀ DANH SÁCH</a>
